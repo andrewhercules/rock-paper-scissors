d5625e12cacdf7317b86082a05cf8ad436c3da43
Andrew Hercules <andrewhercules@gmail.com>
2014-10-04 12:14:07 +0100

    Updated Rock-Paper-Scissors.rb file to fix params and variable issues on index.erb and newgame.erb views

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -1,16 +1,46 @@
 require 'sinatra/base'
+require_relative 'game'
+require_relative 'player'
 
 class RockPaperScissors < Sinatra::Base
 
 	set :views, Proc.new { File.join(root, '..', 'views') }
+	enable :sessions
+
+	GAME = Game.new
 
 	get '/' do
 		erb :index
 	end
 
+	get '/player' do
+		erb :player
+	end
+
+	post '/player' do
+		player_name = params[:player_1]
+	    PLAYER = Player.new(player_name)
+	    PLAYER.name = player_name
+	    session[:me] = PLAYER
+	    GAME.add_player(PLAYER)
+	    puts GAME.inspect
+	  	erb :player
+	  	redirect '/newgame' 
+	end
+
 	get '/newgame' do
+		@player_name = params[:player_1]
 		erb :newgame
 	end
+
+	post '/result' do
+		player_selection = params[:player_selection]
+		PLAYER.make_selection(player_selection)
+		GAME.accept_player_selection(PLAYER)
+		puts GAME.inspect
+    "Result page goes here!"
+  end
+
 
 	# start the server if ruby file executed directly
 	run! if app_file == $0
